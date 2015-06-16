26acfbe138563e0f4841fcc797fc1cabe3b59ccd
plonghi <pietro.longhi@gmail.com>
2015-06-16 19:33:27 -0400

    created branchpoint class; implemented full branch point structure for A-type lie algebras

--- loom/trivialization.py
+++ loom/trivialization.py
@@ -1,5 +1,5 @@
 from api import load_config
-from geometry import SWData, get_ramification_points
+from geometry import SWData, get_ramification_points, RamificationPoint
 import sympy
 import matplotlib.pyplot as plt
 import cmath
@@ -14,49 +14,106 @@ N_PATH_TO_BP = 100
 PROXIMITY_THRESHOLD = 0.05
 
 
-config = load_config('../default.ini')
-sw = SWData(config)
-ramification_points = get_ramification_points(sw, config['accuracy'])
+
+class BranchPoint:
+    def __init__(self, z=None, structure=None, trivialization=None):
+        self.z = z
+        self.pairs = structure[0]
+        self.singles = structure[1]
+        self.sheets_at_branch_point = structure[2]
+        self.trivialization = trivialization
+        self.positive_root = trivialization.positive_root(structure)
+        self.sheet_tracks_from_basepoint = trivialization.track_sheets_to_bp(z)
+        
+    
 
 
 class Trivialization:
+    """
+    The trivialization class.
+
+    Arguments
+    ---------
+    
+    sw_data : 
+    an object of the type SWData, whose attribute 'curve' 
+    should correspond to the curve in the FIRST fundamental
+    representation of the Lie algebra
+    
+    ramification_points : 
+    a list of objects of the type RamificationPoint, corresponding
+    to the given sw_data.
+
+    lie_algebra : 
+    the Lie algebra associated with the cover, expressed
+    as a list of a capital letter and the rank, e.g.
+    ['A', 3] for the A_3 algebra.
+
+    Attributes
+    ----------
+
+    basepoint : 
+    the base point of the trivialization
+
+    reference_sheets :
+    a list of pairs [i, x] where 'i' is an integer label
+    for the sheet, and 'x' is its position in the fiber of T^*C 
+    over the basepoint
+
+    """
     ### NOTE: I am assuming that branch points do not overlap vertically
     ### this should be guaranteed by introducing an automatic rotation of 
     ### the z-plane before calling this class.
-    ### NOTE: I am assuming the ramification_points are all branch points 
+    ### NOTE: I am only analyzing the ramification_points that are branch points 
     ### for now, not irregular singularities.
-    def __init__(self, sw_data, ramification_points):
+    ### NOTE: I am restricting to square-root type branch points.
+    def __init__(self, sw_data, ramification_points, lie_algebra):
         self.sw_data = sw_data
-        self.ramification_points = ramification_points
-        r_points_z = [r.z for r in self.ramification_points]
-        max_distance = max([abs(x - y) for x in r_points_z 
-                                                    for y in r_points_z])
+        self.algebra = lie_algebra
+        self.branch_points = []
+        self.sheet_weight_dictionary = None
+
+        b_points = [r for r in ramification_points if not r.is_puncture]
+        b_points_z = [b.z for b in b_points]
+        max_distance = max([abs(x - y) for x in b_points_z 
+                                                    for y in b_points_z])
         
         ### Should generalize to take into account punctures as well?
-        r_center = sum(r_points_z)
-        self.basepoint = r_center - 1j * max_distance
+        b_center = sum(b_points_z)
+        self.basepoint = b_center - 1j * max_distance
 
         self.reference_sheets = [[i, x] \
                     for i, x in enumerate(self.sheets_at_z(self.basepoint))]
+        ### Now that we have determined the reference sheets, 
+        ### we build a weight-sheet dictionary
+        self.sheet_weight_dictionary = self.build_dictionary()
 
+        for i, z_bp in enumerate(b_points_z):
+            bp_structure = self.branch_point_structure(z_bp)
+            self.branch_points.append(BranchPoint(
+                                                    z=z_bp, 
+                                                    structure=bp_structure,
+                                                    trivialization=self
+                                                ))
+
+        ### PRESENT THE DATA FOR DEBUGGING PURPOSES
         print "\nSheets of the cover at z_0 = {}".format(self.basepoint)
         print self.reference_sheets
 
-        for i, z_bp in enumerate(r_points_z):
+        print "\nThe dictionary between sheets and weights:"
+        print self.sheet_weight_dictionary
+
+        for i, bp in enumerate(self.branch_points):
             print "\nroot tracking along the path to branch point #{}".format(i)
-            sheets_path = self.track_sheets_to_bp(z_bp)
-            for sheet_list in sheets_path:
-                data_plot(sheet_list, z_bp)
-            pairs, singles  = self.branch_point_structure(z_bp)
-            print "this is the brach point structure for branch point #{}".format(i)
-            print "pairs = {}".format(pairs)
-            print "singles = {}".format(singles)
-            pass
+            for j, sheet_list in enumerate(bp.sheet_tracks_from_basepoint):
+                data_plot(sheet_list, 'sheet {} tracket to branch point {}'.format(j, i))
+            print "this is the branch point structure for branch point #{}".format(i)
+            print "pairs = {}".format(bp.pairs)
+            print "singles = {}".format(bp.singles)
+            print "positive root = {}".format(bp.positive_root)
+            print "sheets at the branch point = {}".format(bp.sheets_at_branch_point)
 
         
-
-
-
     def sheets_at_z(self, z_0):
         from sympy.abc import x, z
         sw_curve_fiber = self.sw_data.curve.num_eq.subs(z, z_0)
@@ -114,12 +171,7 @@ class Trivialization:
         tracked_sheets = self.track_sheets_to_bp(z_bp)
         sheets_at_bp = [sheet_list[-1] for sheet_list in tracked_sheets]
         enum_sh = [[i, s_i] for i, s_i in enumerate(sheets_at_bp)]
-
-        print "sheets of the cover at z = {}".format(z_bp)
-        print enum_sh
         
-        ### These 'pairs' and 'singles' should be probably
-        ### introduced as attributes of the branch-point class
         pairs = []
         singles = []
 
@@ -146,10 +198,58 @@ class Trivialization:
                 if paired == False:
                     singles.append(i)
 
-        return pairs, singles
+        return pairs, singles, enum_sh
+
+    def build_dictionary(self):
+        algebra = self.algebra
+
+        if algebra[0] == 'A':
+            r = algebra[1]
+            ### for example, fund_weights(2) will be [0, 1, 0, ...]
+            def fund_weights(i):
+                return np.array([kr_delta(j, i - 1) for j in range(r+1)])
+            
+            return {i : fund_weights(i+1) for i, x in self.reference_sheets}
+
+        elif algebra[0] == 'D':
+            raise ValueError('I am not ready for D-type algebras yet!')
+
+        elif algebra[0] == 'E':
+            raise ValueError('I am not ready for E-type algebras yet!')
 
 
+    def positive_root(self, structure):
+        """
+        Determines the positive root associated with 
+        a branch point's 'structure', i.e. how the sheets
+        collide at the branch point
+        """
+        pairs, singles, enum_sh = structure
+        algebra = self.algebra
+        first_pair = pairs[0]
+        i_1 = first_pair[0]
+        i_2 = first_pair[1]
+        v_1 = self.sheet_weight_dictionary[i_1]
+        v_2 = self.sheet_weight_dictionary[i_2]
+        
+        if algebra[0] == 'A':
+            if i_1 < i_2:
+                return v_1 - v_2
+            else:
+                return v_2 - v_1
 
+        elif algebra[0] == 'D':
+            raise ValueError('I am not ready for D-type algebras yet!')
+
+        elif algebra[0] == 'E':
+            raise ValueError('I am not ready for E-type algebras yet!')
+
+
+def kr_delta(i, j):
+    if i == j:
+        return 1
+    else:
+        return 0
 
 def getkey_real(item):
     return item[1].real
@@ -192,7 +292,14 @@ def data_plot(cmplx_list, title):
     plt.show()
 
 
-print "\nThe SW curve"
+### Some Testing
+
+config = load_config('../default.ini')
+sw = SWData(config)
+ramification_points = get_ramification_points(sw, config['accuracy'])
+
+
+print "\nThe SW curve in the FIRST fundamental representation"
 print sw.curve.sym_eq
 print sw.curve.num_eq
 
@@ -200,4 +307,6 @@ print "\nThe ramification points"
 for r in ramification_points:
     print r
 
-t = Trivialization(sw, ramification_points)
+algebra = ['A', 2]
+
+t = Trivialization(sw, ramification_points, algebra)
