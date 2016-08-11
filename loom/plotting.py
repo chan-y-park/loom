727c9bec7303bbe1172b576a361adb0de5f21b91
plonghi <pietro.longhi@gmail.com>
2016-08-11 06:18:12 -0400

    checkpoint

--- loom/plotting.py
+++ loom/plotting.py
@@ -253,139 +253,139 @@ class NetworkPlot(SpectralNetworkPlotBase):
         self.figure.show()
 
 
-# class NetworkPlotTk(SpectralNetworkPlotBase):
-#     """
-#     This class implements UIs using Tkinter.
+class NetworkPlotTk(SpectralNetworkPlotBase):
+    """
+    This class implements UIs using Tkinter.
 
-#     The content of this class is independent of the parent class.
-#     It only depends on the grandparent class, which should be
-#     'NetworkPlotBase'. Therefore this class can inherit any class
-#     whose parent is 'NetworkPlotBase'; just change the name of the
-#     parent in the definition of this class.
-#     """
-#     def __init__(self, master=None, title=None, plot_range=None,):
-#         super(NetworkPlotTk, self).__init__(
-#             matplotlib_figure=matplotlib.figure.Figure(),
-#             plot_range=plot_range,
-#         )
+    The content of this class is independent of the parent class.
+    It only depends on the grandparent class, which should be
+    'NetworkPlotBase'. Therefore this class can inherit any class
+    whose parent is 'NetworkPlotBase'; just change the name of the
+    parent in the definition of this class.
+    """
+    def __init__(self, master=None, title=None, plot_range=None,):
+        super(NetworkPlotTk, self).__init__(
+            matplotlib_figure=matplotlib.figure.Figure(),
+            plot_range=plot_range,
+        )
 
-#         if master is None:
-#             root = tk.Tk()
-#             root.withdraw()
-#             self.root = root
-#         else:
-#             self.root = master
-#         self.master = master
+        if master is None:
+            root = tk.Tk()
+            root.withdraw()
+            self.root = root
+        else:
+            self.root = master
+        self.master = master
 
-#         # Create a Toplevel widget, which is a child of GUILoom
-#         # and contains plots,
-#         self.toplevel = tk.Toplevel(self.root)
-#         self.toplevel.wm_title(title)
-#         self.toplevel.protocol("WM_DELETE_WINDOW", self.on_closing)
+        # Create a Toplevel widget, which is a child of GUILoom
+        # and contains plots,
+        self.toplevel = tk.Toplevel(self.root)
+        self.toplevel.wm_title(title)
+        self.toplevel.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-#         self.plot_idx_scale = None
+        self.plot_idx_scale = None
 
-#         self.plot_idx_entry = None
-#         self.plot_idx_entry_var = tk.StringVar()
-#         self.plot_idx_entry_var.trace('w', self.plot_idx_entry_change)
+        self.plot_idx_entry = None
+        self.plot_idx_entry_var = tk.StringVar()
+        self.plot_idx_entry_var.trace('w', self.plot_idx_entry_change)
 
-#         self.canvas = FigureCanvas(
-#             self.figure,
-#             master=self.toplevel,
-#             resize_callback=self.canvas_resize_callback
-#         )
-#         self.canvas.show()
-#         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.canvas = FigureCanvas(
+            self.figure,
+            master=self.toplevel,
+            resize_callback=self.canvas_resize_callback
+        )
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-#         toolbar = NavigationToolbar(self.canvas, self.toplevel)
-#         toolbar.update()
-#         self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        toolbar = NavigationToolbar(self.canvas, self.toplevel)
+        toolbar.update()
+        self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-#     def on_closing(self):
-#         self.toplevel.destroy()
-#         if self.master is None:
-#             self.root.destroy()
+    def on_closing(self):
+        self.toplevel.destroy()
+        if self.master is None:
+            self.root.destroy()
 
-#     def scale_action(self, scale_value):
-#         new_plot_idx = int(scale_value)
-#         self.update_current_plot(new_plot_idx)
-#         self.plot_idx_entry_var.set(new_plot_idx)
+    def scale_action(self, scale_value):
+        new_plot_idx = int(scale_value)
+        self.update_current_plot(new_plot_idx)
+        self.plot_idx_entry_var.set(new_plot_idx)
 
-#     def plot_idx_entry_change(self, *args):
-#         try:
-#             new_plot_idx = int(self.plot_idx_entry_var.get())
+    def plot_idx_entry_change(self, *args):
+        try:
+            new_plot_idx = int(self.plot_idx_entry_var.get())
 
-#             if new_plot_idx == self.current_plot_idx:
-#                 return None
-#             elif new_plot_idx < 0:
-#                 new_plot_idx = 0
-#             elif new_plot_idx > len(self.plots) - 1:
-#                 new_plot_idx = len(self.plots) - 1
+            if new_plot_idx == self.current_plot_idx:
+                return None
+            elif new_plot_idx < 0:
+                new_plot_idx = 0
+            elif new_plot_idx > len(self.plots) - 1:
+                new_plot_idx = len(self.plots) - 1
 
-#             self.plot_idx_scale.set(new_plot_idx)
-#             self.update_current_plot(new_plot_idx)
+            self.plot_idx_scale.set(new_plot_idx)
+            self.update_current_plot(new_plot_idx)
 
-#         except ValueError:
-#             pass
+        except ValueError:
+            pass
 
-#         return None
+        return None
 
-#     def update_current_plot(self, new_plot_idx):
-#         if self.data_cursor is not None:
-#             self.data_cursor.hide().disable()
+    def update_current_plot(self, new_plot_idx):
+        if self.data_cursor is not None:
+            self.data_cursor.hide().disable()
 
-#         self.plots[self.current_plot_idx].set_visible(False)
-#         self.plots[new_plot_idx].set_visible(True)
-#         # Update the index variable for the currently displayed plot.
-#         self.current_plot_idx = new_plot_idx
-#         self.set_data_cursor()
-#         self.canvas.draw_idle()
-#         self.canvas.get_tk_widget().focus_set()
+        self.plots[self.current_plot_idx].set_visible(False)
+        self.plots[new_plot_idx].set_visible(True)
+        # Update the index variable for the currently displayed plot.
+        self.current_plot_idx = new_plot_idx
+        self.set_data_cursor()
+        self.canvas.draw_idle()
+        self.canvas.get_tk_widget().focus_set()
 
-#         return None
+        return None
 
-#     def canvas_resize_callback(self, event):
-#         self.set_data_cursor()
+    def canvas_resize_callback(self, event):
+        self.set_data_cursor()
 
-#     def show(self):
-#         plot_idx = 0
-#         self.current_plot_idx = plot_idx
-#         self.plots[plot_idx].set_visible(True)
-#         self.set_data_cursor()
+    def show(self):
+        plot_idx = 0
+        self.current_plot_idx = plot_idx
+        self.plots[plot_idx].set_visible(True)
+        self.set_data_cursor()
 
-#         if(len(self.plots) > 1):
-#             tk.Label(
-#                 self.toplevel,
-#                 text='Plot #',
-#             ).pack(side=tk.LEFT)
+        if(len(self.plots) > 1):
+            tk.Label(
+                self.toplevel,
+                text='Plot #',
+            ).pack(side=tk.LEFT)
 
-#             self.plot_idx_entry_var.set(plot_idx)
-#             self.plot_idx_entry = tk.Entry(
-#                 self.toplevel,
-#                 textvariable=self.plot_idx_entry_var,
-#                 width=len(str(len(self.plots) - 1)),
-#             )
-#             self.plot_idx_entry.pack(side=tk.LEFT)
+            self.plot_idx_entry_var.set(plot_idx)
+            self.plot_idx_entry = tk.Entry(
+                self.toplevel,
+                textvariable=self.plot_idx_entry_var,
+                width=len(str(len(self.plots) - 1)),
+            )
+            self.plot_idx_entry.pack(side=tk.LEFT)
 
-#             tk.Label(
-#                 self.toplevel,
-#                 text='/{}'.format(len(self.plots) - 1),
-#             ).pack(side=tk.LEFT)
+            tk.Label(
+                self.toplevel,
+                text='/{}'.format(len(self.plots) - 1),
+            ).pack(side=tk.LEFT)
 
-#             self.plot_idx_scale = tk.Scale(
-#                 self.toplevel,
-#                 command=self.scale_action,
-#                 # length=100*len(self.plots),
-#                 orient=tk.HORIZONTAL,
-#                 showvalue=0,
-#                 to=len(self.plots) - 1,
-#                 variable=self.current_plot_idx,
-#             )
-#             self.plot_idx_scale.pack(
-#                 expand=True,
-#                 fill=tk.X,
-#                 side=tk.LEFT,
-#             )
+            self.plot_idx_scale = tk.Scale(
+                self.toplevel,
+                command=self.scale_action,
+                # length=100*len(self.plots),
+                orient=tk.HORIZONTAL,
+                showvalue=0,
+                to=len(self.plots) - 1,
+                variable=self.current_plot_idx,
+            )
+            self.plot_idx_scale.pack(
+                expand=True,
+                fill=tk.X,
+                side=tk.LEFT,
+            )
 
 
 def get_label(value, dictionary):
